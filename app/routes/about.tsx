ba998b32984b600addafcd911c0c0d2aa30a4552
Brian Cilenti <briancilenti@Brians-MacBook-Pro-2.local>
2022-03-11 23:12:02 -0500

    still works

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -2,18 +2,37 @@ import * as React from "react";
 import { Link } from "remix";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import { Box } from "@mui/material";
 import MyMenu from "~/src/components/MyMenu";
 
+import { useLoaderData, json } from "remix";
+
+type LoaderData = Awaited<ReturnType<typeof getLoaderData>>;
+
+async function getLoaderData() {
+  const products = ["one", "two", "three"];
+  return { products };
+}
+
+export const loader = async () => {
+  return json<LoaderData>(await getLoaderData());
+};
+
 export default function About() {
+  const product = useLoaderData<LoaderData>();
+
   return (
     <React.Fragment>
+      <Box>
+        <MyMenu />
+      </Box>
       <Typography variant="h4" component="h1" gutterBottom>
         Remix with TypeScript example
       </Typography>
+      <pre>{JSON.stringify(product, null, 2) || "nothing to preview"}</pre>
       <Button variant="contained" component={Link} to="/">
         Go to the main page
       </Button>
-      <MyMenu />
     </React.Fragment>
   );
 }
--- app/src/Layout.tsx
+++ app/src/Layout.tsx
@@ -1,17 +1,15 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import ProTip from './ProTip';
-import Copyright from './Copyright';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import ProTip from "./ProTip";
+import Copyright from "./Copyright";
+import Navbar from "./components/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        {children}
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <>
+      <Navbar />
+      <Container maxWidth="lg">{children}</Container>
+    </>
   );
 }
