8083c381555c5f65d47348fb00d648b947bc3c8c
keepforever <briancilenti@gmail.com>
2022-07-01 14:43:41 -0400

    x

--- app/src/Layout.tsx
+++ app/src/Layout.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react'
 import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
-import ProTip from './ProTip'
-import Copyright from './Copyright'
 import Navbar from './components/Navbar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
